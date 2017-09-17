943ae175c0803648876cad3cbc56c37a4b0f6f36
Camden Ko <camdenko@gmail.com>
2017-09-17 16:22:57 -0500

    1.1, 1.2

---
+++ problems/1/1problems.js
@@ -0,0 +1,20 @@
+const {
+  Set,
+  List,
+} = require('immutable')
+
+module.exports = {
+  isUnique: toTest => Set(toTest.split('')).size === toTest.length,
+  checkPermutation: (string1, string2) => {
+    const list1 = List(string1.split('')).sort()
+    const list2 = List(string2.split('')).sort()
+    return list1.equals(list2)
+  },
+  urlIfy: (toTest) => {
+    const toTestList = List(toTest.split(''))
+    const whiteSpaceCount = toTestList
+      .filter(char => char === ' ')
+      .size
+    console.log(whiteSpaceCount)
+  }
+}
---
+++ problems/1/1tests.js
@@ -0,0 +1,61 @@
+const { expect } = require('chai')
+const {
+  isUnique,
+  checkPermutation,
+  urlIfy,
+} = require('./1problems')
+
+describe('Chapter 1, [], ""', () => {
+  describe('1.1 isUnique', () => {
+    it('returns true for null string', () => {
+      const test = ''
+      expect(isUnique(test)).to.be.equal(true)
+    })
+    it('returns true for same character but different case', () => {
+      const test = 'aA'
+      expect(isUnique(test)).to.be.equal(true)
+    })
+    it('returns false for a string with repeated characters', () => {
+      const test = 'AA'
+      expect(isUnique(test)).to.be.eql(false)
+    })
+    it('returns false for duplicated special chars', () => {
+      const test = '&asdkfjnwn&'
+      expect(isUnique(test)).to.be.eql(false)
+    })
+    it('returns true for a long unique string with special chars', () => {
+      const test = 'jkIP19+_'
+      expect(isUnique(test)).to.be.eql(true)
+    })
+  })
+
+  describe('1.2 checkPermutation', () => {
+    it('returns true if both strings are empty', () => {
+      const strings = ['', '']
+      expect(checkPermutation(...strings)).to.be.eql(true)
+    })
+    it('returns true if the strings are one char each', () => {
+      const strings = ['a', 'a']
+      expect(checkPermutation(...strings)).to.be.eql(true)
+    })
+    it('returns true with complex strings with special chars', () => {
+      const strings = ['Ab123$', '321$Ab']
+      expect(checkPermutation(...strings)).to.be.eql(true)
+    })
+    it('returns false when strings are different', () => {
+      const strings = ['ASDF@R@#R', '23423rf']
+      expect(checkPermutation(...strings)).to.be.eql(false)
+    })
+  })
+
+  describe('1.3 urlIfy', () => {
+    it('returns null string for null string', () => {
+      const string = ''
+      expect(urlIfy(string)).to.be.eql(string)
+    })
+    it('properly replaces when one space', () => {
+      const string = 'url 1  '
+      expect(urlIfy(string)).to.be.eql('url%201')
+    })
+  })
+})
