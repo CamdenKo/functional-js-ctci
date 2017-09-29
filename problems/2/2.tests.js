07b79ee71b7efc86d4e164f920773b7bd470d345
Camden Ko <camdenko@gmail.com>
2017-09-29 10:52:43 -0500

    added expect to globals eslint

--- problems/2/2.tests.js
+++ problems/2/2.tests.js
@@ -2,7 +2,19 @@ import * as CTCI from './2problems'
 import LinkedList from './2.utilities'
 
 describe('Chapter 2, Linked Lists', () => {
+  describe('LinkedList tests', () => {
+    describe('length', () => {
+      it('returns 1 for a single node', () => {
+        const head = new LinkedList(1)
+        expect(head.length).to.be.eql(1)
+      })
+    })
+  })
+
   describe('2.1 removeDups', () => {
-    it('runs on an empty linked list')
+    it('returns a linkedList without duplicates', () => {
+      const head = new LinkedList(1)
+      head.next = new LinkedList(1)
+    })
   })
 })
--- problems/2/2.utilities.js
+++ problems/2/2.utilities.js
@@ -3,4 +3,7 @@ export default class LinkedList {
     this.value = value
     this.next = next
   }
+
+  length = () =>
+    this.next ? 1 + this.next.length() : 1
 }
