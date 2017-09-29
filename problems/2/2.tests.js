2175624dff36a7fb9128ca7284642e8202f53b7d
Camden Ko <camdenko@gmail.com>
2017-09-29 10:40:22 -0500

    simpleLinkedList

---
+++ problems/2/2.tests.js
@@ -0,0 +1,7 @@
+import * as CTCI from './2problems'
+
+describe('Chapter 2, Linked Lists', () => {
+  describe('2.1 removeDups', () => {
+    it('runs on an empty linked list')
+  })
+})
---
+++ problems/2/2.utitilites.js
@@ -0,0 +1,6 @@
+export default class LinkedList {
+  constructor(value = null, next = null) {
+    this.value = value
+    this.next = next
+  }
+}
