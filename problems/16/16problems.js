d767a84a4444d0e16b08d4a15ca06fb512e3c6d8
Camden Ko <camdenko@gmail.com>
2017-10-11 16:55:27 -0500

    16.2 single run

--- problems/16/16problems.js
+++ problems/16/16problems.js
@@ -4,6 +4,23 @@ const numberSwapper = (arr) => {
   arr[0] -= arr[1]
 }
 
+const wordFrequencies = (fullText, target) =>
+  fullText
+    .split(' ')
+    .filter(ele => ele)
+    .reduce(
+      (accum, curValue) => Object.assign(
+        accum,
+        {
+          [curValue]: accum[curValue]
+            ? accum[curValue] + 1
+            : 1,
+        },
+      ),
+      {},
+    )[target] || 0
+
 module.exports = {
   numberSwapper,
+  wordFrequencies,
 }
